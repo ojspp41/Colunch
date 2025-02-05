79514251d43468488049d00123a456bd093bc19f
ojspp41 <72492122+ojspp41@users.noreply.github.com>
2025-02-05 21:16:54 +0900

    Revert "지울거 지운코드 ! 충전하기를 누르면 모달창이 누르게 수정 모달창 오픈 닫기 props로 관리했음"

--- src/components/PointBalance.jsx
+++ src/components/PointBalance.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import '../css/components/PointBalance.css'; // 스타일을 분리하여 유지보수 가능하게 설정
 import { useNavigate } from 'react-router-dom'; // React Router 사용
-import MainPaymentModal from './MainPaymentModal';
-const PointBalance = ({ amount }) => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
 
-  
+const PointBalance = ({ amount }) => {
+    const navigate = useNavigate(); // 네비게이션 함수
+
+  const handleChargeClick = () => {
+    navigate('/charge'); // "/charge" 경로로 이동
+  };
   return (
     <div className="point-balance">
       <div className="left-section">
@@ -13,8 +15,7 @@ const PointBalance = ({ amount }) => {
         <span className="point-text">보유 포인트</span>
         <span className="amount">{amount} P</span>
       </div>
-      <button className="charges-button" onClick={() => setIsModalOpen(true)}>충전하기</button>
-      {isModalOpen && <MainPaymentModal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />}
+      <button className="charges-button" onClick={handleChargeClick}>충전하기</button>
     </div>
   );
 };
