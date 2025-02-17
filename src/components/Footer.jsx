5df5e3ccbc1b05fd2bde1b7103872020b00eb45b
ohjunseok <ojspp000@naver.com>
2025-02-17 20:39:21 +0900

    메인페이지 수정중

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className={styles.footer}>
       <div className={styles.footer}>
-        Developed By COMA, Catholic University of Korea
+      대표이사 천승환 | 호스팅서비스사업자 코매칭 | 사업자 등록번호843-27-01742 | 통신판매신고 2024-부천원미-2812 | 대표전화  >010-3039-7387 | 경기도 부천시 조마루로 366번길 27, 401호
       </div>
     </div>
   );
