012ff5f8e70bd82b31432980d9fc7741ff9c998e
ojspp41 <ojspp000@naver.com>
2025-03-17 21:45:55 +0900

    loading 바 수정 ~!

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -1,50 +1,35 @@
 import React, { useState, useEffect } from "react";
 import "../css/pages/Loading.css";
-import HeaderNav from "../components/HeaderNav";
+import HeaderLogin from "../components/HeaderLogin";
 import { useNavigate } from "react-router-dom";
-
+import Background from "../components/Background";
 const Loading = () => {
   const [offset, setOffset] = useState(-100);
   const navigate = useNavigate();
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setOffset((prevOffset) => (prevOffset < 100 ? prevOffset + 1 : -100));
-    }, 15);
+    
     const redirectTimeout = setTimeout(() => {
       navigate("/match-result");
 
-    }, 2000);
+    }, 3000);
 
     return () => {
-      clearInterval(interval);
       clearTimeout(redirectTimeout);
     };
   }, [navigate]);
 
   return (
     <div className="container">
-      <HeaderNav />
-      <div className="circle-icon">💟</div>
-      <div className="content">
+      <Background/>
+      <HeaderLogin />
+      <div className="contents">
         <div className="LoadingText">
           코매칭 AI가 입력하신 결과를 바탕으로 <br />
           비슷한 매칭 상대를 찾고 있어요..
         </div>
-        <div className="LoadingBar">
-          <div
-            className="GradientBar firstloadingbar"
-            style={{ backgroundPosition: `${offset}% 0` }}
-          />
-          <div
-            className="GradientBar secondloadingbar"
-            style={{ backgroundPosition: `${offset}% 0` }}
-          />
-          <div
-            className="GradientBar thirdloadingbar"
-            style={{ backgroundPosition: `${offset}% 0` }}
-          />
-        </div>
+        
+        
       </div>
     </div>
   );
