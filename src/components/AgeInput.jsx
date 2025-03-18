49bcc217df888bed992f8bab0424a4a509f7d29c
ojspp41 <ojspp000@naver.com>
2025-03-18 14:31:59 +0900

    알림수정정

--- src/components/AgeInput.jsx
+++ src/components/AgeInput.jsx
@@ -9,7 +9,7 @@ function AgeInput({ value, onChange }) {
 
     // 유효성 검사
     if (!/^(19|20|21|22|23|24|25|26|27|28|29)$/.test(value)) {
-      errorMessage = "나이는 20부터 29까지의 숫자로 입력하세요.";
+      errorMessage = "나이는 19부터 29까지의 숫자로 입력하세요.";
       onChange({ target: { name, value: "" } });
     }
 
